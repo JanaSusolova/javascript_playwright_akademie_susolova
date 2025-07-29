eb0f1384da0c58d570f4b72b281e7d800631c845
JanaSusolova <150517140+JanaSusolova@users.noreply.github.com>
2025-07-29 20:30:19 +0200

    Vytvořena kalkulačka

---
+++ hw4_calculator.js
@@ -0,0 +1,24 @@
+function secist(a, b) {
+  const result = a + b;
+  console.log("Výsledek sčítání: " + result);
+}
+
+function odecist(a, b) {
+  const result = a - b;
+  console.log("Výsledek odečítání: " + result);
+}
+
+function vynasobit(a, b) {
+  const result = a * b;
+  console.log("Výsledek násobení: " + result);
+}
+
+function vydelit(a, b) {
+  const result = a / b;
+  console.log("Výsledek dělení: " + result);
+}
+
+secist(565, 584);
+odecist(474, 520);
+vynasobit(5822, 65);
+vydelit(632, 6);
